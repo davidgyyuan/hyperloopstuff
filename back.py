7336f429d7269d28e2ad788f5bfc69b8367b83e0
David Y <you@example.com>
2017-07-31 19:37:02 -0500

    Sending data now meets SpaceX binary telemetry frame requirements :)

--- back.py
+++ back.py
@@ -1,23 +1,26 @@
+from Tkinter import *
 import Tkinter
 import ttk
 import threading
+import struct
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 
-measurementList = "Distance Speed Acceleration".split()
-defaultValues = "0 0 0".split()
+measurementList = "Distance Speed Acceleration - - - - - - -".split()
+defaultValues = [0] * len(measurementList)
 
-PORT_NUMBER = 6006
+PORT_NUMBER = 3000
 SIZE = 1024
 hostName = gethostbyname('0.0.0.0')
 mySocket = socket(AF_INET, SOCK_DGRAM)
 mySocket.bind((hostName, PORT_NUMBER))
-
+SERVER_IP = ''
 
 class Frame(Tkinter.Frame):
     def __init__(self, parent):
         Tkinter.Frame.__init__(self, parent)
         self.parent = parent
         self.tree = ttk.Treeview(self.parent, columns='data')
+        self.button = Button(self.parent, text="Emergency Stop", command=cmdStop)
         self.initialize_user_interface()
         self.init_data()
 
@@ -31,7 +34,8 @@ class Frame(Tkinter.Frame):
         self.tree.heading('#1', text='Data')
         self.tree.column("#0", stretch=Tkinter.YES)
         self.tree.column("#1", stretch=Tkinter.YES, anchor='e')
-        self.tree.grid(row=0, columnspan=1, sticky='nsew')
+        self.tree.grid(row=0, columnspan=1, sticky='new')
+        self.button.grid(row=1, columnspan=1, sticky='n')
 
     def init_data(self):
         for i in range(len(measurementList)):
@@ -39,17 +43,24 @@ class Frame(Tkinter.Frame):
 
 
 def updateData(frame, newData):
-    for i in range(len(newData)):
+    for i in range(len(measurementList)):
             frame.tree.set(i+1, column="#1", value=newData[i])
 
 
+def cmdStop():
+    mySocket.sendto('s'*35, (SERVER_IP, PORT_NUMBER))
+
+
 def count():
+    global SERVER_IP
     while not root.state() == 'normal':
         pass
     while root.state() == 'normal':
-        (data, addr) = mySocket.recvfrom(SIZE)
-        receivedList = data.split()
+        unpacker = struct.Struct('! B B i i i i i i i I')
+        (data, addr) = mySocket.recvfrom(unpacker.size)
+        receivedList = unpacker.unpack(data)
         mainFrame.parent.title("512 Hyperloop Display Panel: Client Connected")
+        SERVER_IP = addr
         updateData(mainFrame, receivedList)
 
 root = Tkinter.Tk()
--- cli.py
+++ cli.py
@@ -1,17 +1,30 @@
 import random
-import time
+import time, struct
 from socket import socket, AF_INET, SOCK_DGRAM
 
 
 SERVER_IP = raw_input("Server IP: ")
-PORT_NUMBER = 6006
+PORT_NUMBER = 3000
 SIZE = 1024
 
-mySocket = socket( AF_INET, SOCK_DGRAM )
+mySocket = socket(AF_INET, SOCK_DGRAM)
+mySocket.connect((SERVER_IP, PORT_NUMBER))
+
+def fillData():
+    n = 10
+    inputdata = ()
+    for i in range(n):
+        inputdata += (int(random.random()*10),)
+    for i in range(10 - len(inputdata)):
+        inputdata += (0,)
+    packer = struct.Struct('! B B i i i i i i i I')
+    packeddata = packer.pack(*inputdata)
+    return packeddata
 
 while True:
-    data = ""
-    for i in range(3):
-        data = data + str(random.random()) + " "
-    mySocket.sendto(data,(SERVER_IP,PORT_NUMBER))
-    time.sleep(1)
+    data = fillData()
+    try:
+        mySocket.sendall(data)
+    except:
+        pass
+    time.sleep(.2)
